872c06d7872fdffeef9ad61a0deaeda9f65cdf22
Hannah <reillyhannah65@gmail.com>
2020-02-11 15:25:27 -0500

    routes

--- adoption/config/routes.rb
+++ adoption/config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-    resources :adoptions
-    resources :dogs
-    resources :users
-    resources :login
-    resources :register
+    resources :dogs do
+        resources :application
+    end
     post 'auth/login', to: 'authentication#authenticate'
     post 'signup', to: 'users#create'
     end

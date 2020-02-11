0f95b0abe5981b762ccd4632c51f157f60cd16ca
Hannah <reillyhannah65@gmail.com>
2020-02-11 14:02:09 -0500

    app.js changes

--- adawpt/src/App.js
+++ adawpt/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import './App.css';
 import {loginUser, registerUser, verifyUser} from './services/api-helper';
 import LoginForm from './components/Login';

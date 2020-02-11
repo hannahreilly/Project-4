235e27191527bcb058d1d87e5f260e86b37eb1ee
Hannah <reillyhannah65@gmail.com>
2020-02-11 14:34:41 -0500

    adoptions

---
+++ adawpt/src/components/adoption.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Adoption = (props) => {
+    const { img, name, age, breed } = props.adoption.pet
+    return (
+        <div>
+            <div className = "adoption-content">
+                <div className = "adoption-img-wrapper">
+                    <img className="adoption-img"src = {img}/>
+                </div>
+            <h4> <span>Pet Name: </span> {name} </h4>
+            <h4> <span>Pet Breed: </span> {breed} </h4>
+            <h4> <span>Pet Age: </span> {age} </h4>
+            </div>
+        </div>
+    )
+}
+
+export default Adoption;

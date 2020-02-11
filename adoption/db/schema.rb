dc2e5574cd72d9d76ade3ad89de723cddb96e7d0
Hannah <reillyhannah65@gmail.com>
2020-02-11 15:27:28 -0500

    schema

--- adoption/db/schema.rb
+++ adoption/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_10_181658) do
+ActiveRecord::Schema.define(version: 2020_02_11_201137) do
 
   create_table "adopts", force: :cascade do |t|
     t.integer "dog_id", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_02_10_181658) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "adopts", "adopts", column: "adopts_id"

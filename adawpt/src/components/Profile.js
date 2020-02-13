079809b734212ed97afe3ca9a1b5a1cc104406ea
Hannah <reillyhannah65@gmail.com>
2020-02-13 16:54:13 -0500

    profile

--- adawpt/src/components/Profile.js
+++ adawpt/src/components/Profile.js
@@ -8,7 +8,6 @@ class Profile extends Component {
     name: "",
     username: "",
     location: "", 
-    gender: ""
     
  }
 
@@ -22,10 +21,9 @@ class Profile extends Component {
     })
   }
 
-  handleOnChange = (event) => {
-    console.log(event.target.value)
+  handleOnChange = (e) => {
     this.setState({
-      [event.target.name]: event.target.value
+      [e.target.name]: e.target.value
     })
   }
 
@@ -55,27 +53,6 @@ class Profile extends Component {
       
   }
 
-  renderGenderPhoto = ()=>{
-    if (this.props.user.gender === "Female"){
-      return  <img className="profile-img" 
-      src="https://cdn1.iconfinder.com/data/icons/avatars-1-5/136/87-512.png"
-       alt={this.props.user.name}/> 
-    } else if( this.props.user.gender === "Male"){
-      
-       return <img className="profile-img" 
-        src="https://cdn0.iconfinder.com/data/icons/finance-1-8/151/25-512.png"
-         alt={this.props.user.name}/> 
-      
-    } else  if (this.props.user.gender === "Prefered not to say"){
-     return  <img className="profile-img" 
-        src="https://cdn3.iconfinder.com/data/icons/vector-icons-6/96/256-512.png"
-         alt={this.props.user.name}/> 
-    } else {
-      return  <img className="profile-img" 
-      src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Question_mark_alternate.svg"
-       alt={this.props.user.name}/>
-    }
-  }
 
   handleDelete = ()=>{
     fetch(`http://localhost:3000/users/${this.props.user.id}`,{
@@ -96,49 +73,25 @@ class Profile extends Component {
   
         <label htmlFor="location">Location:</label>
         <input type="text" id="location" name="location" value={this.state.location}/>
-  
-        <label htmlFor="gender">Gender:</label>
-        <select name="gender">
-          <option value="default">Your prefered gender</option>
-          <option value="Female">
-            Female
-          </option>
-          <option value="Male">
-            Male
-          </option>
-          <option value="Prefered not to say">
-            Prefered not to say
-          </option>
-        </select>
     
         <input type="submit" onClick={(event) => this.handleOnSubmit(event)}/>
     </form>)
   }
 
   profile = ()=>{
-    const { name, location, username, gender} = this.props.user
     return (<div className="profile-div">
-         {this.renderGenderPhoto()}
          <div className="profile-info">
           <h2>
-            {name}
+            {this.props.currentUser.name}
           </h2>
-
           <p> 
             <b className="bold">Location:</b> 
-            {location}
+            {this.props.currentUser.location}
           </p>
-        
           <p> 
             <b className="bold">Username:</b> 
-            {username}
+            {this.props.currentUser.username}
           </p>
-    
-          <p> 
-            <b className="bold">Gender:</b> 
-            {gender}
-          </p>
-
           <button onClick={this.handleEditButton} className="profile-edit-button">
             { this.state.isEditFormOn?
               "Profile" : "Edit me"
@@ -150,21 +103,23 @@ class Profile extends Component {
   }
  
  render() {
-  if (localStorage.loggedInUserId){
+  if (localStorage.name){
     return(
       <div className="profile-item">
-       { this.state.isEditFormOn ? 
-          this.editForm()
-           :
-          this.profile()
-       }
-     </div>
+        <p>hello</p>
+     
+      <div className="name">
+      <h4>{this.props.name}</h4>
+      </div>
+
+      <div className="location">
+      <h4>{this.props.location}</h4>
+      </div>
+
+      </div>
       )
-     } else
-       return (
-         <> 
-         </>
-     )
   }
  }
+}
+
 export default Profile;

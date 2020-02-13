5cbdb51b783a243612935131ad362c540a212d9b
Hannah <reillyhannah65@gmail.com>
2020-02-13 16:53:34 -0500

    feed

--- adawpt/src/components/Feed.js
+++ adawpt/src/components/Feed.js
@@ -1,89 +1,141 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-
-class Feed extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      users: false,
-      apiDataLoaded: false,
-      search: null,
-      filteredUsers: []
-    }
-  }
-
-  componentDidMount = async () => {
-    try {
-      const userResponse = await axios.get(`https://localhost:3000`);
-      this.setState({
-        users: userResponse.data,
-        apiDataLoaded: true
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  handleSearchChange = async (e) => {
-    const { value } = e.target;
-    await this.setState({
-      search: value
-    })
-    this.filterUsers()
-  }
-
-  filterUsers = () => {
-    let filteredUsers = this.state.users.filter(user =>
-      user.first_name.toLowerCase().startsWith(this.state.search.toLowerCase())
-    )
-    this.setState({
-      filteredUsers
-    })
-  }
-
-  addDefaultSrc(e) {
-    e.target.src = 'http://www.racemph.com/wp-content/uploads/2016/09/profile-image-placeholder.png'
-  }
-
-  render() {
-    const linkStyle = {
-      textDecoration: "none",
-    }
-
-    return (
-      <div>
-          <div>
-            <div className="feedpage-wrapper">
-              <div className="feedpage-content">
-                {this.state.apiDataLoaded && this.state.search &&
-                  this.state.filteredUsers.map(user => (
-                    <Link style={linkStyle} to={`/profile/${user.id}`} key={user.id}>
-                      <div key={user.id} className="users">
-                        <h4>{user.name}</h4>
-                        <img onError={this.addDefaultSrc} src={user.profile_pic_url} alt="user" />
-                      </div>
-                    </Link>
-                  ))
-                }
-                {this.state.apiDataLoaded && !this.state.search &&
-                  this.state.users.map(user => (
-                    <Link style={linkStyle} to={`/profile/${user.id}`} key={user.id}>
-                      <div key={user.id} className="users">
-                        <h4>{user.name}</h4>
-                        <img onError={this.addDefaultSrc} src={user.profile_pic_url} alt="user" />
-                      </div>
-                    </Link>
-                  ))}
-              </div>
-            </div >
-          </div>
-        
-      </div>
-    )
-  }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Feed;
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Album layout
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Browse Available Dogs
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              Something short and leading about the collection below—its contents, the creator, etc.
+              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+              entirely.
+            </Typography>
+            <div className={classes.heroButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random/dog"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}

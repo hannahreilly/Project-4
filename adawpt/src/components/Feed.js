d99e9379055b3f85db66051aacb7ee979e493e15
Hannah <reillyhannah65@gmail.com>
2020-02-14 16:14:20 -0500

    feed

--- adawpt/src/components/Feed.js
+++ adawpt/src/components/Feed.js
@@ -58,12 +58,21 @@ const useStyles = makeStyles(theme => ({
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Album() {
+export default function Album(props) {
   const classes = useStyles();
 
   return (
+    
     <React.Fragment>
       <CssBaseline />
+      <div>
+      {props.dogs && props.dogs.map(dog => (
+        <div key={dog.id}>
+          <Link to={`/dogs/${dog.id}`}><h3>{dog.title}</h3></Link>
+        </div>
+      ))}
+      <Link to="/dogs/new"><button>Add a dog</button></Link>
+    </div>
       {/* <AppBar position="relative">
         <Toolbar>
           <CameraIcon className={classes.icon} />
@@ -80,9 +89,7 @@ export default function Album() {
               Browse Available Dogs
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+            “Saving one dog will not change the world, but surely for that one dog, the world will change forever.”
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">

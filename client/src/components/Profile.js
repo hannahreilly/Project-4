d25496376a863e0e598240de381e051035e420f1
Hannah <reillyhannah65@gmail.com>
2020-02-18 16:48:08 -0500

    profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -4,11 +4,11 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+// import EditableLabel from 'react-inline-editing';
+
 // import Footer from '../components/Footer';
 
 
@@ -64,15 +64,15 @@ export default function Album() {
                     image="https://source.unsplash.com/random"
                     title="Image title"
                   />
+                  
                   <CardContent className={classes.cardContent}>
-            
-                    <Typography>
+
                       <div className="profile-details">
                       <span>Your Name</span>
                       <span>Your Location</span>
                       <span>Your preferred breeds</span>
                       </div>
-                    </Typography>
+                   
                   </CardContent>
                   <CardActions>
                     <Button size="small">
